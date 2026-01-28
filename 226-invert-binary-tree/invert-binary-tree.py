484e55a1360e710fa977267e2ee1e4eecabb4421
Sophia Wang <104732494+s8phia@users.noreply.github.com>
2026-01-28 11:42:42 -0500

    Time: 0 ms (100.00%) | Memory: 19.3 MB (35.71%) - LeetSync

--- 226-invert-binary-tree/invert-binary-tree.py
+++ 226-invert-binary-tree/invert-binary-tree.py
@@ -11,5 +11,6 @@ class Solution:
         root.left, root.right = root.right, root.left
         self.invertTree(root.left)
         self.invertTree(root.right)
+        
         return root
             
